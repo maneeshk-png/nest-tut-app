5f5ee65beca6f8aa5fcb4b8cb95053473c8e397b
maneesh kumar <maneesh@unicourt.com>
2026-02-03 14:24:49 +0530

    feat:Learned Logs,Debugging and Env related concepts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [ UsersModule],
+  imports: [ConfigModule.forRoot({isGlobal: true, // optional: makes config available in the entire app
+    }), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,11 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException,Logger } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
+
 @Injectable()
 export class UsersService {
-
+    private readonly logger = new Logger(UsersService.name);
     //private json data
     private users = [
         {
@@ -72,9 +73,13 @@ export class UsersService {
       
       //FindAll Method 
       findAll(role?:'INTERN' |'ENGINEER' |'ADMIN'){
+        this.logger.log(`Fetching all users${role ? ' with role ' + role : ''}`);
         if(role){
             const UsersArray= this.users.filter(user=>user.role===role);
-            if(UsersArray.length===0) throw new NotFoundException('User Role Not Found');
+            if(UsersArray.length===0){
+                 throw new NotFoundException('User Role Not Found');
+            }
+            this.logger.debug(`Found ${UsersArray.length} users with role ${role}`);
             return UsersArray;
         }
       return this.users;
@@ -82,15 +87,20 @@ export class UsersService {
 
      //find One
      findOne(id:number){
+        this.logger.log(`fetching user with id ${id}`)
         const user=this.users.find(user=>user.id===id);
 
-        if(!user) throw new NotFoundException('User not found');
-
+        if(!user){ 
+            this.logger.warn(`User not Found ${id}`)
+          throw new NotFoundException('User not found');
+        }
+        this.logger.debug(`User found: ${JSON.stringify(user)}`);
         return user;
      }
 
      //create user
      create(CreateUserDto:CreateUserDto){
+        this.logger.log(`creating user with data ${JSON.stringify(CreateUserDto)}`)
         const usersByHgihestId=[...this.users].sort((a,b)=>b.id-a.id);
 
         const newUser={
@@ -98,25 +108,32 @@ export class UsersService {
             ...CreateUserDto
         }
         this.users.push(newUser);
+        this.logger.debug(`User Created Successfully ${JSON.stringify(newUser)}`)
         return newUser;
      }
 
      //update
      update(id:number,updateUserDto:UpdateUserDto){
+        
         this.users=this.users.map(user=>{
             if(user.id==id){
                 return {...user,...updateUserDto};
             }
             return user
         })
-        return this.findOne(id);
+        const updatedUser = this.findOne(id);
+        this.logger.debug(`User updated successfully: ${JSON.stringify(updatedUser)}`);
+        return updatedUser;
      }
 
      //delete user
 
      delete(id:number){
+        this.logger.log(`Deleting user with id ${id}`);
         const deletedUser= this.findOne(id);
         this.users=this.users.filter(user=>user.id!=id);
+        
+        this.logger.debug(`User deleted successfully: ${JSON.stringify(deletedUser)}`);
         return deletedUser;
      }
 
