d19a30a393b38eb80010fe28dce1af29543f810f
maneesh kumar <maneesh@unicourt.com>
2026-02-03 11:42:28 +0530

    feat:Understand the Service Cocnept

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,42 +1,42 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+
 
 @Controller('users')
 export class UsersController {
 
+    constructor(private userService:UsersService){}
+
+
     //Get All Users
   @Get()
-  findAll(){
-    return [];
+  findAll(@Query('role')role?:'INTERN' |'ENGINEER'|'ADMIN'){
+    return this.userService.findAll(role);
   }
 
   //Get user By id
   @Get(':id')
   findOne(@Param('id')id:string){
-    return {id};
+    return this.userService.findOne(+id);
   }
 
   //Create User
   @Post()
-  create(@Body()user:{}){
-    return user;
+  create(@Body()user:{name:string,email:string,role:'INTERN' |'ENGINEER' | 'ADMIN'}){
+    return this.userService.create(user);
   }
 
   //Patch User
   @Patch(':id')
-  update(@Param('id')id:string,@Body() userUpdate: {}){
-    return {id,...userUpdate};
+  update(@Param('id')id:string,@Body() userUpdate: {name?:string,email?:string,role?:'INTERN'|'ENGINEER'|'ADMIN'}){
+    return this.userService.update(+id,userUpdate);
   }
 
   //Delete
   @Delete(':id')
    delete(@Param('id') id:string){
     return {id};
-  }
-
-  //Based On Query
-  @Get()
-  GetAllUserByQuery(@Query('role')role?:'INTERN' |'ENGINEER'|'ADMIN'){
-    return [];
   }
 
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,4 +3,114 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class UsersService {
 
+    //private json data
+    private users = [
+        {
+          "id": 1,
+          "name": "Alice Johnson",
+          "email": "alice.johnson@example.com",
+          "role": "ADMIN"
+        },
+        {
+          "id": 2,
+          "name": "Bob Smith",
+          "email": "bob.smith@example.com",
+          "role": "ENGINEER"
+        },
+        {
+          "id": 3,
+          "name": "Charlie Brown",
+          "email": "charlie.brown@example.com",
+          "role": "INTERN"
+        },
+        {
+          "id": 4,
+          "name": "Diana Prince",
+          "email": "diana.prince@example.com",
+          "role": "ADMIN"
+        },
+        {
+          "id": 5,
+          "name": "Ethan Hunt",
+          "email": "ethan.hunt@example.com",
+          "role": "ENGINEER"
+        },
+        {
+          "id": 6,
+          "name": "Fiona Gallagher",
+          "email": "fiona.gallagher@example.com",
+          "role": "INTERN"
+        },
+        {
+          "id": 7,
+          "name": "George Martin",
+          "email": "george.martin@example.com",
+          "role": "ENGINEER"
+        },
+        {
+          "id": 8,
+          "name": "Hannah Baker",
+          "email": "hannah.baker@example.com",
+          "role": "INTERN"
+        },
+        {
+          "id": 9,
+          "name": "Ian McKellen",
+          "email": "ian.mckellen@example.com",
+          "role": "ADMIN"
+        },
+        {
+          "id": 10,
+          "name": "Julia Roberts",
+          "email": "julia.roberts@example.com",
+          "role": "ENGINEER"
+        }
+      ]
+      
+      
+      //FindAll Method 
+      findAll(role?:'INTERN' |'ENGINEER' |'ADMIN'){
+        if(role){
+            return this.users.filter(user=>user.role===role);
+        }
+      return this.users;
+     }
+
+     //find One
+     findOne(id:number){
+        const user=this.users.find(user=>user.id===id);
+        return user;
+     }
+
+     //create user
+     create(user:{name:string,email:string,role:'INTERN' |'ENGINEER' | 'ADMIN'}){
+        const usersByHgihestId=[...this.users].sort((a,b)=>b.id-a.id);
+
+        const newUser={
+            id:usersByHgihestId[0].id+1,
+            ...user
+        }
+        this.users.push(newUser);
+        return newUser;
+     }
+
+     //update
+     update(id:number,updateUser:{name?:string,email?:string,role?:'INTERN'|'ENGINEER'|'ADMIN'}){
+        this.users=this.users.map(user=>{
+            if(user.id==id){
+                return {...user,...updateUser};
+            }
+            return user
+        })
+        return this.findOne(id);
+     }
+
+     //delete user
+
+     delete(id:number){
+        const deletedUser= this.findOne(id);
+        this.users=this.users.filter(user=>user.id!=id);
+        return deletedUser;
+     }
+
 }
